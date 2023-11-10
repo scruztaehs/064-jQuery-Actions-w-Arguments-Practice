54c8250df5971cc2d050f2fb8b835ee6fd913366
scruztaehs <75856359+scruztaehs@users.noreply.github.com>
2023-11-10 13:15:28 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,12 @@
+// Guided Practice
+$(".reply-yes").click(function() {  
+    $(".homers-reply").text("Woo Hoo!");
+    $(".reply-yes").css("background-color", "yellow");
+});
+
+//Independent Practice
+$(".replyNo").click(function() {  
+      
+    
+});
+
